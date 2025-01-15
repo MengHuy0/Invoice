bbf9da1d52fb758d216a630d2ac9e1fb1288a23d
MengHuy0 <menghuy0000@gmail.com>
2025-01-15 20:09:19 +0700

    final landing page

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+const btnPopup = document.querySelector('.btnLogin-popup');
+const cover_box = document.querySelector('.cover_box');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const iconClose = document.querySelector('.icon-close');
+function activateCoverBox() {
+   event.preventDefault();
+    cover_box.classList.add('active');
+};
+function deactivateCoverBox() {
+    cover_box.classList.remove('active');
+};
+function activePopup() {
+    cover_box.classList.add('active-popup');
+};
+function deactivateCoverPopup() {
+    cover_box.classList.remove('active-popup');
+};
+registerLink.addEventListener('click', activateCoverBox);
+loginLink.addEventListener('click', deactivateCoverBox);
+btnPopup.addEventListener('click', activePopup);
+iconClose.addEventListener('click', deactivateCoverPopup);
+
+
+// img slider
 const slides = document.querySelectorAll(".slides img")
 let slideIndex = 0;
 let intervalId = null;
